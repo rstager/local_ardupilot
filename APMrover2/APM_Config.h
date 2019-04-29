7cf4bf85be4e63defb1f0793c3f5af4d2315988e
rstager <roger@stager.us>
2019-04-28 21:31:04 -0700

    Enable FRSKY telem

--- APMrover2/APM_Config.h
+++ APMrover2/APM_Config.h
@@ -1,4 +1,4 @@
 // This file is just a placeholder for your configuration file.  If you wish to change any of the setup parameters from
 // their default values, place the appropriate #define statements here.
 
-//#define FRSKY_TELEM_ENABLED ENABLED
+#define FRSKY_TELEM_ENABLED ENABLED

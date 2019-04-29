31682aa1e1b53052fee3046170fedb3ae016606d
rstager <roger@stager.us>
2019-04-28 19:36:11 -0700

    Fix compiler error for float compare == 0.0

--- libraries/AP_L1_Control/AP_L1_Control.cpp
+++ libraries/AP_L1_Control/AP_L1_Control.cpp
@@ -236,7 +236,7 @@ void AP_L1_Control::update_waypoint(const struct Location &prev_WP, const struct
 
     //Calculate groundspeed
     float groundSpeed = _groundspeed_vector.length();
-    if (groundSpeed == 0.0){
+    if (groundSpeed < 0.001){
         _latAccDem = 0;
         return;  // This should never happen in real life, but does happen in simulator.
         // If we are not moving, just ignore this.
